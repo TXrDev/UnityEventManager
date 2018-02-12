5224885e1e75f81cc8feed32fc6111def43122c1
TXrDev <txr.mail@gmail.com>
2018-02-12 09:43:06 +0200

    Event Manager w|w/o passing game object.

---
+++ EventManager.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using System.Collections.Generic;
+
+public class EventManager : MonoBehaviour
+{
+    [Serializable]
+    public class UnityEventGo : UnityEvent<GameObject>
+    {
+    }
+
+    private Dictionary<string, UnityEvent> eventDictionary;
+    private Dictionary<string, UnityEventGo> eventDictionaryGo;
+
+    private static EventManager eventManager;
+
+    public static EventManager instance
+    {
+        get
+        {
+            if (!eventManager)
+            {
+                eventManager = FindObjectOfType(typeof(EventManager)) as EventManager;
+
+                if (!eventManager)
+                {
+                    Debug.LogError("There needs to be one active EventManger script on a GameObject in your scene.");
+                }
+                else
+                {
+                    eventManager.Init();
+                }
+            }
+
+            return eventManager;
+        }
+    }
+
+    void Init()
+    {
+        if (eventDictionary == null)
+        {
+            eventDictionary = new Dictionary<string, UnityEvent>();
+        }
+        if (eventDictionaryGo == null)
+        {
+            eventDictionaryGo = new Dictionary<string, UnityEventGo>();
+        }
+    }
+
+    public static void StartListening(string eventName, UnityAction listener)
+    {
+        UnityEvent thisEvent = null;
+        if (instance.eventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.AddListener(listener);
+        }
+        else
+        {
+            thisEvent = new UnityEvent();
+            thisEvent.AddListener(listener);
+            instance.eventDictionary.Add(eventName, thisEvent);
+        }
+    }
+
+    public static void StartListeningGo(string eventName, UnityAction<GameObject> listener)
+    {
+        UnityEventGo thisEvent = null;
+        if (instance.eventDictionaryGo.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.AddListener(listener);
+        }
+        else
+        {
+            thisEvent = new UnityEventGo();
+            thisEvent.AddListener(listener);
+            instance.eventDictionaryGo.Add(eventName, thisEvent);
+        }
+    }
+
+    public static void StopListening(string eventName, UnityAction listener)
+    {
+        if (eventManager == null) return;
+        UnityEvent thisEvent = null;
+        if (instance.eventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.RemoveListener(listener);
+        }
+    }
+
+    public static void StopListeningGo(string eventName, UnityAction<GameObject> listener)
+    {
+        if (eventManager == null) return;
+        UnityEventGo thisEvent = null;
+        if (instance.eventDictionaryGo.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.RemoveListener(listener);
+        }
+    }
+
+    public static void TriggerEvent(string eventName)
+    {
+        UnityEvent thisEvent = null;
+        if (instance.eventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.Invoke();
+        }
+    }
+
+    public static void TriggerEventGo(string eventName, GameObject value)
+    {
+        UnityEventGo thisEvent = null;
+        if (instance.eventDictionaryGo.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.Invoke(value);
+        }
+    }
+}
